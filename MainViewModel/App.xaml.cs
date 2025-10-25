ca0c99d7ea0c7863aa390fd55d2cf227b1c72d03
xX_DarkLorD_Xx <yuzekesu@gmail.com>
2025-10-25 17:07:25 +0200

    * Managed to share the resource between the projects. The description of how it does is documented in the "Learning.txt" file.

--- MainViewModel/App.xaml.cs
+++ MainViewModel/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace MainViewModel
+namespace Othello.ViewModel
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainViewModel/MainWindow.xaml.cs
+++ MainViewModel/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MainViewModel
+namespace Othello.ViewModel
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
